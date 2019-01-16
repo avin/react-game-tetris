9bb50dda7bf78bad3804e54e3823c58f16b43aa0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-16 19:04:49 +0000

    Deploy avin/react-game-tetris to github.com/avin/react-game-tetris.git:gh-pages

---
+++ precache-manifest.b61197b9102c54de83b15d07832a8ae5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ed072ceabc09ae3416",
+    "url": "./static/css/main.6192a234.chunk.css"
+  },
+  {
+    "revision": "42ed072ceabc09ae3416",
+    "url": "./static/js/main.42ed072c.chunk.js"
+  },
+  {
+    "revision": "e5b89ec96c43d0e12611",
+    "url": "./static/css/1.2f25b985.chunk.css"
+  },
+  {
+    "revision": "e5b89ec96c43d0e12611",
+    "url": "./static/js/1.e5b89ec9.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "a601a008859e9f5a09bf56d28738c45b",
+    "url": "./index.html"
+  }
+];
